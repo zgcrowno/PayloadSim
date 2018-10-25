700987ceaab307f9e91f61abbf1c3a57f836c1f5
Zachary Crownover <zgcrowno@gmail.com>
2018-10-25 18:36:08 -0500

    Doing more work towards ui layout and functionality

--- Assets/Scripts/Tab.cs
+++ Assets/Scripts/Tab.cs
@@ -4,59 +4,107 @@ using UnityEngine;
 
 public class Tab : MonoBehaviour {
 
-    public GameObject header;
+    public World world; //The overarching GameObject of which all others are children
 
-    public TabHeader headerScript;
-    public Camera mainCam;
-    public Vector2 position;
-    public LineRenderer border;
+    public GameObject parentTab; //If null, this tab's parent is the screen, and if not null, this tab is nested in another
 
-    public float width;
-    public float height;
+    public Rect headerRect;
+    public Rect bodyRect;
+
+    public Rect[] nestedTabs = new Rect[8];
+
+    public Stack<Vector2> prevPos = new Stack<Vector2>();
+
+    public int depth; //Integer representing the order in which different Tabs will be drawn, thus allowing for overlay
+
+    public float octantWidth;
+    public float octantHeight;
+
+    public float[][] nestedTabOctants = new float[8][];
+    
+    public bool beingDragged; //Represents whether or not this tab is currently being dragged by the player
 
 	// Use this for initialization
 	void Start () {
-        //Initialize the Tab object's corresponding TabHeader object
-        header = new GameObject();
-        headerScript = header.AddComponent<TabHeader>();
+        world = GameObject.Find("/World").GetComponent<World>();
+        parentTab = null;
+        //headerRect = new Rect(0, 0, Screen.width / 6, Screen.height / 20);
+        //bodyRect = new Rect(0, Screen.height / 20, Screen.width, Screen.height - (Screen.height / 20));
 
-        //All Tab objects will need to make use of the main camera
-        mainCam = Camera.main;
+        octantWidth = bodyRect.width / 4;
+        octantHeight = (bodyRect.height / 2);
 
-        //Vector2 representing the center of the Tab object
-        position = new Vector2(0.5f, 0.5f);
+        //Initialize nestedTabs
+        for (int i = 0; i < nestedTabs.Length; i++)
+        {
+            float x;
+            float y;
 
-        //Add and setup the LineRenderer component by which the Tab object's border is rendered
-        border = gameObject.AddComponent<LineRenderer>();
-        border.positionCount = 4; //Four vertices
-        AnimationCurve borderWidth = new AnimationCurve();
-        borderWidth.AddKey(0, 0.02f);
-        borderWidth.AddKey(1, 0.02f);
-        border.widthCurve = borderWidth;
-        border.startColor = Color.magenta;
-        border.endColor = Color.magenta;
-        border.loop = true;
+            if (i < 4)
+            {
+                x = i * octantWidth;
+                y = bodyRect.y;
+            }
+            else
+            {
+                x = (i - 4) * octantWidth;
+                y = bodyRect.y + octantHeight;
+            }
+            nestedTabs[i] = new Rect(x, y, octantWidth, octantHeight);
+        }
+    }
 
-        //Initialize the Tab object's width and height in viewport-relative values (0 is no width/height, and 1 is viewport-sized width/height)
-        width = 1;
-        height = 1;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //Update the Tab object's corresponding TabHeader object
-        headerScript.width = width;
-        headerScript.height = 0.02f;
-        headerScript.background.startWidth = height;
-        headerScript.background.endWidth = height;
-        headerScript.position = new Vector2(position.x, position.y + (height / 2));
+    // Update is called once per frame
+    void Update () {
+        
+    }
 
-        //Must set z-axis value to a positive number so that the lines are visible within the camera's frustum
-        border.SetPosition(0, mainCam.ViewportToWorldPoint(new Vector3(position.x - (width / 2), position.y - (height / 2), 1)));
-        border.SetPosition(1, mainCam.ViewportToWorldPoint(new Vector3(position.x - (width / 2), position.y + (height / 2), 1)));
-        border.SetPosition(2, mainCam.ViewportToWorldPoint(new Vector3(position.x + (width / 2), position.y + (height / 2), 1)));
-        border.SetPosition(3, mainCam.ViewportToWorldPoint(new Vector3(position.x + (width / 2), position.y - (height / 2), 1)));
-        headerScript.background.SetPosition(0, mainCam.ViewportToWorldPoint(new Vector3(headerScript.position.x - (headerScript.width / 2), headerScript.position.y, 1)));
-        headerScript.background.SetPosition(1, mainCam.ViewportToWorldPoint(new Vector3(headerScript.position.x + (headerScript.width / 2), headerScript.position.y, 1)));
+    private void OnGUI()
+    {
+        //Set the depth of this Tab in order to achieve the correct drawing order
+        GUI.depth = depth;
+
+        //Draw the tab's header
+        GUI.DrawTexture(headerRect, Texture2D.whiteTexture, ScaleMode.ScaleAndCrop, true, 0, Color.white, 0, 5);
+
+        //Draw the tab's body
+        GUI.DrawTexture(bodyRect, Texture2D.whiteTexture, ScaleMode.ScaleAndCrop, true, 0, Color.white, 1, 0);
+
+        //Draw the nestedTabs for debugging purposes
+        foreach (Rect rect in nestedTabs)
+        {
+            GUI.DrawTexture(rect, Texture2D.blackTexture, ScaleMode.ScaleAndCrop, true, 0);
+            GUI.DrawTexture(rect, Texture2D.whiteTexture, ScaleMode.ScaleAndCrop, true, 0, Color.white, 1, 0);
+        }
+
+        //headerRect interactivity
+        if (headerRect.Contains(Event.current.mousePosition)) //The mouse cursor is within the bounds of headerRect
+        {
+            print(depth);
+            if (Event.current.type == EventType.MouseDown)
+            {
+                world.SetTabDepths(this);
+                beingDragged = true;
+            }
+        }
+        if (Event.current.type == EventType.MouseUp)
+        {
+            beingDragged = false;
+
+            //Snap to previous position
+            Vector2 prevPosBody = prevPos.Pop();
+            Vector2 prevPosHeader = prevPos.Pop();
+            headerRect.x = prevPosHeader.x;
+            headerRect.y = prevPosHeader.y;
+            bodyRect.x = prevPosBody.x;
+            bodyRect.y = prevPosBody.y;
+            prevPos.Push(new Vector2(headerRect.x, headerRect.y));
+            prevPos.Push(new Vector2(bodyRect.x, bodyRect.y));
+        }
+        if (depth == 0 && beingDragged && Event.current.type == EventType.MouseDrag)
+        {
+            headerRect.x = Event.current.mousePosition.x - (headerRect.width / 2);
+            headerRect.y = Event.current.mousePosition.y - (headerRect.height / 2);
+        }
     }
 }
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+
+    public int numTabs = 8;
+
+    public GameObject[] tabs = new GameObject[8];
+
+	// Use this for initialization
+	void Start () {
+        //Initialize the Tab objects
+		for(int i = 0; i < numTabs; i++)
+        {
+            tabs[i] = new GameObject("Tab" + i);
+            Tab tab = tabs[i].AddComponent<Tab>();
+            tab.depth = tabs.Length - 1 - i;
+            tab.headerRect = new Rect(i * (Screen.width / 8), 0, Screen.width / 8, Screen.height / 20);
+            tab.bodyRect = new Rect(0, Screen.height / 20, Screen.width, Screen.height - (Screen.height / 20));
+            tab.prevPos.Push(new Vector2(tab.headerRect.x, tab.headerRect.y));
+            tab.prevPos.Push(new Vector2(tab.bodyRect.x, tab.bodyRect.y));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Method which sets one tab's hasFocus variable to true, while setting all other tabs' hasFocus variable to false
+    public void SetTabDepths(Tab tab)
+    {
+        int otherTabDepth = 1;
+        for(int i = tabs.Length - 1; i >= 0; i--)
+        {
+            if (tabs[i].name.Equals(tab.gameObject.name))
+            {
+                tabs[i].GetComponent<Tab>().depth = 0;
+            }
+            else
+            {
+                tabs[i].GetComponent<Tab>().depth = otherTabDepth;
+                otherTabDepth++;
+            }
+        }
+    }
+}
