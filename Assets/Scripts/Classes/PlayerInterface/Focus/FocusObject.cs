ec647666c347c39c1657af3e9f8b373531ad0a4f
Zachary Crownover <zgcrowno@gmail.com>
2018-12-14 12:38:03 -0600

    Moved around 3D game objects within SampleScene, imported and instantiated some standard assets for testing, and added very small rotation script to focus object

---
+++ Assets/Scripts/Classes/PlayerInterface/Focus/FocusObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(0, 0.5f, 0), Space.World);
+    }
+}
